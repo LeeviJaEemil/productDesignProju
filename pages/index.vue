43273d878bf7c35c1dcf5b5db83c81c9b7e176ea
LeeviIlli <t1ille00@students.oamk.fi>
2023-11-08 13:21:15 +0200

    yläpalkkia

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
-    <div>
-        <MainLayout>
-            <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-                IndexPage 
-            </div>
-        </MainLayout>
-    </div>
-    <footer>footer</footer>
+  <div>
+    <MainLayout>
+      <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+        IndexPage
+      </div>
+    </MainLayout>
+  </div>
 </template>
 
 <script setup>
-import MainLayout from '~/layouts/MainLayout.vue';
+import MainLayout from "~/layouts/MainLayout.vue";
 </script>
