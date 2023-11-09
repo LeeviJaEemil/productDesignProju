59daf5884027c423d76902af4a18f6ac92bb7386
eduvaa <eemiliki@gmail.com>
2023-11-10 00:49:15 +0200

    uudelleen nimettujä tiedostoja ja typoja

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,14 +1,15 @@
 <template>
     <MainLayout>
-        <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+        <div id="CartPage" class="bg-[#A6CF98] mt-4 max-w-[1200px] mx-auto px-2">
             <div v-if="true" class="h-[500px] flex items-center justify-center">
                 <div class="pt-20">
-                    <img class="mx-auto" width="250" src="/shoppingcart.png">
                     
-                    <div class="text-xl text-center mt-4">
-                        
-                    </div>
-
+                    <img 
+                    class="mx-auto"
+                    width="250" 
+                    src="/cart.png"
+                    >
+                    
                     <div class="text-xl text-center mt-4">no items yet?</div>
 
                     <div v-if="true" class="flex text-center">
