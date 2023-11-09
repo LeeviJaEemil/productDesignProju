03e1436e88d366e6b285453abc835437ab03e7a9
eduvaa <eemiliki@gmail.com>
2023-11-09 17:07:43 +0200

    typon  korjaus

--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -1,7 +1,7 @@
 <template>
     <MainLayout>
         <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            <div v-if="ture" class="h-[500px] flex items-center justify-center">
+            <div v-if="true" class="h-[500px] flex items-center justify-center">
                 <div class="pt-20">
                     <img class="mx-auto" width="250" src="/shoppingcart.png">
                     
