dd273ebbcb750f0eb88d9bbfadafc7f622cfb4d5
LeeviIlli <t1ille00@students.oamk.fi>
2023-11-14 12:26:05 +0200

    Auth pagea ja navbar toimii

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,93 +1,85 @@
 <template>
   <div
     id="MainLayout"
-    class="w-full border-b-2 border-[#557C55] md:block hidden"
+    class="justify-between w-full border-b-2 border-[#557C55]"
   >
     <div
-      class="flex justify-between items-center text-xs font-light h-9 bg-[#F2FFE9] mx-auto"
+      class="flex items-center text-xs font-light h-9 bg-[#F2FFE9] px-2 relative"
     >
-      <!-- Left side content -->
-      <div class="ml-2">
-        <nuxt-link to="/" class="flex items-center">
-          <img width="30" src="tonttu.png" />
+      <button @click="showSearch = !showSearch" class="md:hidden">
+        <Icon name="ph:magnifying-glass" size="15" />
+      </button>
 
-          <h1 class="static font-bold">Tontun paja</h1>
-        </nuxt-link>
-      </div>
+      <nuxt-link
+        to="/"
+        class="flex-grow flex items-center justify-center md:justify-start"
+      >
+        <img width="30" src="tonttu.png" alt="Logo" />
+        <h1 class="static font-bold ml-2">Tontun paja</h1>
+      </nuxt-link>
 
-      <!-- Center content (Search field) -->
-      <div class="flex items-center">
-        <div
-          name="hakukenttä"
-          class="static max-w-[550px] w-full mr-2 md:block hidden"
-        >
+      <button @click="toggleMenu" class="md:hidden">
+        <Icon name="mdi:menu" size="20" />
+      </button>
+
+      <div class="hidden md:flex ml-2 items-center space-x-4">
+        <div class="flex">
           <div
-            class="flex items-center border-2 bg-[#A6CF98] border-[#557C55] rounded-md"
+            name="hakukenttä"
+            class="render-fixed static w-[95%] mr-2 md:block hidden"
           >
-            <input
-              v-model="searchItem"
-              type="search"
-              class="fw-full w-[550px] placeholder-[#557C55] bg-[#A6CF98] text-sm pl-3 focus:outline-none"
-              placeholder="Search..."
-            />
-            <Icon
-              v-if="isSearching"
-              name="eos-icons:loading"
-              size="15"
-              class="mr-2"
-            />
+            <div
+              class="flex items-center border-2 bg-[#A6CF98] border-[#557C55] rounded-md"
+            >
+              <input
+                v-model="searchItem"
+                type="search"
+                class="fw-full w-[500px] placeholder-[#557C55] bg-[#A6CF98] text-sm pl-3 focus:outline-none"
+                placeholder="Search..."
+              />
+              <Icon
+                v-if="isSearching"
+                name="eos-icons:loading"
+                size="15"
+                class="mr-2"
+              />
 
-            <button class="flex items-center h-[100%] p-1.5 px-3 bg-[#557C55]">
-              <Icon name="ph:magnifying-glass" size="15" color="#fff" />
-            </button>
-          </div>
-
-          <div class="absolute bg-white max-w-[550px] h-auto w-full">
-            <div v-if="false" class="p-1">
-              <nuxt-link
-                to="'/item/1'"
-                class="flex items-cener justify-between w-full cursor-pointer hover:bg-gray-100"
+              <button
+                class="flex items-center h-[100%] p-1.5 px-3 bg-[#557C55]"
               >
-                <div class="flex items-center">
-                  <img
-                    class="rounded-md"
-                    width="40"
-                    src="https://picsum.photos/id82/300/320"
-                  />
-                  <div class="truncate ml-2">Testi</div>
-                </div>
-                <div class="truncate">100 €</div>
-              </nuxt-link>
+                <Icon name="ph:magnifying-glass" size="15" color="#fff" />
+              </button>
+            </div>
+
+            <div class="absolute bg-white max-w-[550px] h-auto w-full">
+              <div v-if="false" class="p-1">
+                <nuxt-link
+                  to="'/item/1'"
+                  class="flex items-cener justify-between w-full cursor-pointer hover:bg-gray-100"
+                >
+                  <div class="flex items-center">
+                    <img
+                      class="rounded-md"
+                      width="40"
+                      src="https://picsum.photos/id82/300/320"
+                    />
+                    <div class="truncate ml-2">Testi</div>
+                  </div>
+                  <div class="truncate">100 €</div>
+                </nuxt-link>
+              </div>
             </div>
           </div>
         </div>
-        <!-- Search field -->
-        <!-- (Place your search field code here) -->
-      </div>
-      <!-- Right side content (Cart and Account) -->
-      <ul class="flex items-center">
-        <li
-          class="border-r-[1px] border-r-black px-1 hover:text-[#557C55] cursor-pointer"
+        <nuxt-link
+          to="/cart"
+          class="flex items-center border-r-[1px] border-r-black px-1 hover:text-[#557C55]"
         >
-          <nuxt-link to="/cart" class="flex items-center">
-            <div class="min-w-[40pxs]">
-              <Icon
-                name="ph:shopping-cart-simple-light"
-                size="15"
-                class="ml-1"
-              />
-              <span
-                class="flex items-center justify-center bg-[#FA7070] h-[13px] min-w-[13px] text-xs text-black px-0.5 rounded-full"
-              >
-                100</span
-              >
-            </div>
-          </nuxt-link>
-        </li>
-        <li
-          @mouseenter="isAccountMenu = true"
-          @mouseleave="isAccountMenu = false"
-          class="h-[30px] relative flex items-center px-1 hover:text-[#557C55] cursor-pointer"
+          <Icon name="ph:shopping-cart-simple-light" class="mr-2" size="15" />
+        </nuxt-link>
+        <div
+          @click="toggleAccouuntMenu"
+          class="h-[30px] relative flex items-center hover:text-[#557C55] cursor-pointer"
         >
           <h1>Account</h1>
           <Icon name="ph:user-thin" size="17" class="ml-1" />
@@ -95,29 +87,52 @@
           <div
             id="AccountMenu"
             v-if="isAccountMenu"
-            class="absolute top-[30px] text-[#333333]"
+            class="absolute right-0 mt-2 bg-white shadow-md w-40 py-2 top-[30px] text-[#333333]"
           >
-            <div class="bg-white">
-              <NuxtLink to="/auth"> Login</NuxtLink>
-            </div>
-            <div>
-              <ul>
-                <!-- <li @click="navigateTo('#')" v-if="true">2</li>
-                <li @click="navigateTo('#')" v-if="true">3</li> -->
-              </ul>
-            </div>
+            <NuxtLink to="/auth">Login</NuxtLink>
           </div>
-        </li>
-      </ul>
+        </div>
+      </div>
+
+      <div
+        v-if="isMenuOpen"
+        class="absolute right-0 top-full mt-2 bg-white shadow-md w-40 py-2 md:hidden"
+      >
+        <!-- Valikon sisältö -->
+        <nuxt-link to="/cart" class="block px-4 py-1 hover:bg-gray-100"
+          >Ostoskori</nuxt-link
+        >
+        <nuxt-link to="/auth" class="block px-4 py-1 hover:bg-gray-100"
+          >Kirjaudu</nuxt-link
+        >
+      </div>
+    </div>
+
+    <div v-if="showSearch" class="md:hidden w-full bg-[#F2FFE9] px-2 py-2">
+      <div
+        class="flex items-center border-2 bg-[#A6CF98] border-[#557C55] rounded-md"
+      >
+        <input
+          v-model="searchItem"
+          type="search"
+          class="w-full placeholder-[#557C55] bg-[#A6CF98] text-sm pl-3 focus:outline-none"
+          placeholder="Search..."
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { useUserStore } from "~/stores/user";
+let isMenuOpen = ref(false);
+let showSearch = ref(false);
 let isAccountMenu = ref(false);
-let isSearching = ref(true);
 let searchItem = ref("");
-const userStore = useUserStore();
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+const toggleAccouuntMenu = () => {
+  isAccountMenu.value = !isAccountMenu.value;
+};
 </script>
--- pages/auth.vue
+++ pages/auth.vue
@@ -2,60 +2,60 @@
   <MainLayout />
   <div
     id="AuthPage"
-    class="flex justify-center items-center h-screen space-x-4 bg-[#A6CF98]"
+    class="flex flex-col md:flex-row justify-center items-center h-screen bg-gray-100 space-x-0 md:space-x-4 space-y-4 md:space-y-0"
   >
     <!-- -->
-    <div class="bg-white p-4 rounded-lg shadow-md max-w-xs w-full">
-      <h2 class="text-lg font-semibold mb-4">Kirjaudu sisään</h2>
+    <div class="bg-[#A6CF98] p-4 rounded-lg shadow-md max-w-xs w-full">
+      <h2 class="text-lg font-semibold mb-4">Log in</h2>
       <form @submit.prevent="login" class="space-y-2">
         <input
           type="email"
           v-model="loginEmail"
-          placeholder="Sähköposti"
+          placeholder="Email"
           class="w-full px-2 py-1 border border-gray-300 rounded-md"
         />
         <input
           type="password"
           v-model="loginPassword"
-          placeholder="Salasana"
+          placeholder="Password"
           class="w-full px-2 py-1 border border-gray-300 rounded-md"
         />
         <button
           type="submit"
-          class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+          class="w-full bg-[#557C55] hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
         >
-          Kirjaudu
+          Log in
         </button>
       </form>
     </div>
 
     <!-- Rekisteröintikortti -->
-    <div class="bg-white p-4 rounded-lg shadow-md max-w-xs w-full">
-      <h2 class="text-lg font-semibold mb-4">Luo käyttäjä</h2>
+    <div class="bg-[#A6CF98] p-4 rounded-lg shadow-md max-w-xs w-full">
+      <h2 class="text-lg font-semibold mb-4">Create account</h2>
       <form @submit.prevent="register" class="space-y-2">
         <input
           type="email"
           v-model="registerEmail"
-          placeholder="Sähköposti"
+          placeholder="Email"
           class="w-full px-2 py-1 border border-gray-300 rounded-md"
         />
         <input
           type="password"
           v-model="registerPassword"
-          placeholder="Salasana"
+          placeholder="Password"
           class="w-full px-2 py-1 border border-gray-300 rounded-md"
         />
         <input
           type="password"
           v-model="confirmPassword"
-          placeholder="Vahvista salasana"
+          placeholder="Confirm password"
           class="w-full px-2 py-1 border border-gray-300 rounded-md"
         />
         <button
           type="submit"
-          class="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
+          class="w-full bg-[#557C55] hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
         >
-          Rekisteröidy
+          Create account
         </button>
       </form>
     </div>
