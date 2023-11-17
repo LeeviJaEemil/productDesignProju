d57ae9b3d9ac976079123f511e4ccc12cb87841a
LeeviIlli <t1ille00@students.oamk.fi>
2023-11-17 16:49:07 +0200

    product database conn tehty ja testattu

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,40 @@
 <template>
   <MainLayout />
   <div id="IndexPage" class="bg-[#A6CF98] mx-auto px-2">
-    <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-      <div v-if="products" v-for="product in products" :key="product">
-        <ProductComponent :product="product"/>
-        {{ product }}
+    <div
+      class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
+    >
+      <div v-if="products.length" v-for="product in products" :key="product.id">
+        <div class="product-card">
+          <img :src="product.url" :alt="product.title" class="product-image" />
+          <div class="product-info">
+            <h3>{{ product.title }}</h3>
+            <p>${{ (product.price / 100).toFixed(2) }}</p>
+          </div>
+        </div>
       </div>
+      <div v-else>Loading products...</div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "~/firebaseConfig";
 import MainLayout from "~/layouts/MainLayout.vue";
 
-const products = [
-  {id: 1, title: "Title 1", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 9999 },
-  {id: 2, title: "Title 2", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 9929 },
-  {id: 3, title: "Title 3", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 9449 },
-  {id: 4, title: "Title 4", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 9669 },
-  {id: 5, title: "Title 5", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 1919 },
-  {id: 6, title: "Title 6", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 6969 },
-  {id: 7, title: "Title 7", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 9912 },
-  {id: 8, title: "Title 8", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 9123 },
-  {id: 9, title: "Title 9", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 9119 },
-  {id: 10, title: "Title 10", description: " this is descccc", url: "https://picsum.photos/id/7/800/800", price: 1299 },
-]
+const products = ref([]);
+
+onMounted(async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "products"));
+    products.value = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Error fetching products: ", error);
+  }
+});
 </script>
