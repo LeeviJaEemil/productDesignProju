39570561fea46864db90e4f5d5aaaed0b67af4bf
LeeviIlli <t1ille00@students.oamk.fi>
2023-11-22 15:15:05 +0200

    logo databasen kautta.     Indeksiin tuuotteiden kuvien vaihtuminen.     Tuotesivu toimii.

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,23 @@
       class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
     >
       <div v-if="products.length" v-for="product in products" :key="product.id">
-        <div class="product-card">
-          <img :src="product.url" :alt="product.title" class="product-image" />
-          <div class="product-info">
-            <h3>{{ product.title }}</h3>
-            <p>${{ (product.price / 100).toFixed(2) }}</p>
+        <nuxt-link :to="'/item/' + product.id">
+          <div
+            class="product-card"
+            @mouseover="startImageRotation(product)"
+            @mouseleave="stopImageRotation"
+          >
+            <img
+              :src="getProductImageUrl(product)"
+              :alt="product.title"
+              class="product-image"
+            />
+            <div class="product-info">
+              <h3>{{ product.title }}</h3>
+              <p>${{ (product.price / 100).toFixed(2) }}</p>
+            </div>
           </div>
-        </div>
+        </nuxt-link>
       </div>
       <div v-else>Loading products...</div>
     </div>
@@ -19,12 +29,13 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "~/firebaseConfig";
 import MainLayout from "~/layouts/MainLayout.vue";
 
 const products = ref([]);
+const activeProduct = reactive({ id: null, imageIndex: 0, interval: null });
 
 onMounted(async () => {
   try {
@@ -37,4 +48,26 @@ onMounted(async () => {
     console.error("Error fetching products: ", error);
   }
 });
+
+function startImageRotation(product) {
+  stopImageRotation(); // Pysäytä mahdollinen aikaisempi ajastin
+  activeProduct.id = product.id;
+  activeProduct.imageIndex = 0;
+  activeProduct.interval = setInterval(() => {
+    activeProduct.imageIndex =
+      (activeProduct.imageIndex + 1) % product.url.length;
+  }, 2500);
+}
+
+function stopImageRotation() {
+  clearInterval(activeProduct.interval);
+  activeProduct.id = null;
+}
+
+function getProductImageUrl(product) {
+  if (activeProduct.id === product.id) {
+    return product.url[activeProduct.imageIndex];
+  }
+  return product.url;
+}
 </script>
--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -1,91 +1,79 @@
-
 <template>
-    <MainLayout/>
-        <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            <div class="md:flex gap-4 justify-between mx-auto w-full">
-                <div class="md:w-[40%]">
-                    <img
-                        v-if="currentImage"
-                        class="rounded-lg object-fit"
-                        :src="currentImage" 
-                    > 
-                    <div v-if="images[0] !== ''" class="flex items-center justify-center mt-2">
-                        <div v-for="image in images">
-                            <img 
-                                @mouseover="currentImage = image"
-                                @click="currentImage = image"
-                                width="70"
-                                class="rounded-md object-fit border-[3px] cursor-pointer"
-                                :class="currentImage === image ? 'border-[#FF5353]' : ''"
-                                :src="image"
-                            >
-                        </div>
-                    </div>
-
-                    
-
-                    <button 
-                        @click="addToCart()"
-                        :disabled="isInCart"
-                        class="
-                            px-6 
-                            py-2 
-                            rounded-lg 
-                            text-white 
-                            text-lg 
-                            font-semibold 
-                            bg-gradient-to-r 
-                            from-[#557C55] 
-                            to-[#A6CF98]
-                        "
-                    >
-                        <div v-if="isInCart">Is Added</div>
-                        <div v-else>Add to Cart</div>
-                    </button>       
-                </div>
-            </div>
+  <MainLayout />
+  <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+    <div class="md:flex gap-4 justify-between mx-auto w-full">
+      <div class="md:w-[40%]">
+        <img
+          v-if="currentImage"
+          class="rounded-lg object-fit"
+          :src="currentImage"
+        />
+        <div
+          v-if="images[0] !== ''"
+          class="flex items-center justify-center mt-2"
+        >
+          <div v-for="image in images">
+            <img
+              @mouseover="currentImage = image"
+              @click="currentImage = image"
+              width="70"
+              class="rounded-md object-fit border-[3px] cursor-pointer"
+              :class="currentImage === image ? 'border-[#FF5353]' : ''"
+              :src="image"
+            />
+          </div>
         </div>
-    
+
+        <button
+          @click="addToCart()"
+          :disabled="isInCart"
+          class="px-6 py-2 rounded-lg text-white text-lg font-semibold bg-gradient-to-r from-[#557C55] to-[#A6CF98]"
+        >
+          <div v-if="isInCart">Is Added</div>
+          <div v-else>Add to Cart</div>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import MainLayout from '~/layouts/MainLayout.vue';
-import { useUserStore } from '~/stores/user';
-const userStore = useUserStore()
+import MainLayout from "~/layouts/MainLayout.vue";
+import { useUserStore } from "~/stores/user";
+import { onMounted, ref, computed } from "vue";
+import { useRoute } from "vue-router";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
-const route = useRoute()
+const userStore = useUserStore();
+const route = useRoute();
+const firestore = getFirestore();
 
-let product = ref(null)
-let currentImage = ref(null)
-
+let product = ref(null);
+let currentImage = ref(null);
+let images = ref([]);
 
 const isInCart = computed(() => {
-    let res = false
-    userStore.cart.forEach(prod => {
-        if (route.params.id == prod.id) {
-            res = true
-        }
-    })
-    return res
-})
+  return userStore.cart.some((prod) => prod.id === route.params.id);
+});
 
-    onMounted(() => {
-        watchEffect(() => {
-            currentImage.value = 'https://picsum.photos/id/77/800/800'
-            images.value[0] = 'https://picsum.photos/id/77/800/800'
-        })
-    })
+async function fetchProduct() {
+  const docRef = doc(firestore, "products", route.params.id);
+  const docSnap = await getDoc(docRef);
 
-    const images = ref([
-    '',
-    'https://picsum.photos/id/212/800/800',
-    'https://picsum.photos/id/233/800/800',
-    'https://picsum.photos/id/165/800/800',
-    'https://picsum.photos/id/99/800/800',
-    'https://picsum.photos/id/144/800/800',
-])
+  if (docSnap.exists()) {
+    product.value = docSnap.data();
+    currentImage.value = product.value.url[0];
+    images.value = product.value.url;
+  } else {
+    console.log("No such product!");
+  }
+}
+
+onMounted(() => {
+  fetchProduct();
+});
 
 const addToCart = () => {
-    userStore.cart.push(product.value.data)
-}
+  userStore.cart.push(product.value);
+};
 </script>
